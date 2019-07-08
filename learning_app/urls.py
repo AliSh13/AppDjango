3a8354ce89ff023d96484213431d2e0851bb3888
alex <alex-just2200@yandex.ru>
2019-07-08 12:50:32 +0300

    create urls

--- learning_app/urls.py
+++ learning_app/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('learning_app.urls')),
+from django.contrib import admin
+from django.urls import path, re_path
+
+import learning_app.views as views
+
+urlpatterns =[
+    re_path(r'^$', views.index, name ='index'),
 
 ]
