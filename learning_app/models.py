b457d9290803db4999bd6dff1b644f2e0dfa75c7
alex <alex-just2200@yandex.ru>
2019-07-08 12:08:22 +0300

    added models

---
+++ learning_app/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Topic(models.Model):
+    """ Тема, которую изучает пользователь. """
+    name = models.CharField(max_length=200)
+    date_add = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+
+        return self.name
+
+
+class Entry(models.Model):
+    """Информация изученная пользователем по теме"""
+    name_topic = models.ForeignKey(Topic, on_delete=True)
+    text = models.TextField()
+    date_add = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+
+        if len(self.text) > 50 :
+            return self.text[:50] + '...'
+
+        return self.text
