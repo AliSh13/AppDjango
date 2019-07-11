6340b977b4d2e82dc4ae29f9bd0bb7d56d96fc19
alex <alex-just2200@yandex.ru>
2019-07-11 16:32:08 +0300

    added topic page

--- learning_app/urls.py
+++ learning_app/urls.py
@@ -22,5 +22,6 @@ import learning_app.views as views
 urlpatterns =[
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
+    re_path(r'topics/(?P<topic_id>\d+$)', views.topic, name='topic'),
 
 ]
--- learning_app/views.py
+++ learning_app/views.py
@@ -10,3 +10,10 @@ def topics(request):
     topics = Topic.objects.order_by('date_add')
     context = {'topics' : topics}
     return render(request,'learning_app/topics.html', context)
+
+def topic(request, topic_id):
+    """вывод всех записей одной темы"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_add')
+    context = {'topic': topic, 'entries': entries}
+    return render(request,'learning_app/topic.html', context)
