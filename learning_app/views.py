9aae1a5a6e1dad191893f04fa7a738c0781c5799
alex <alex-just2200@yandex.ru>
2019-07-12 11:48:59 +0300

    added new_topic form

--- learning_app/views.py
+++ learning_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 from learning_app.models import Topic, Entry
+from learning_app.forms import TopicForm
 
 def index(request):
     """представление главной страницы"""
@@ -17,3 +21,17 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_add')
     context = {'topic': topic, 'entries': entries}
     return render(request,'learning_app/topic.html', context)
+
+def new_topic(request):
+    """ вывод формы для новой темы """
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        #если произошел POST запрос(отправили данные); обработка данных
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+
+    context = {'form': form}
+    return render(request,'learning_app/new_topic.html', context)
