8ac58124d5a6a41e99118394c701ba9d9558d937
alex <alex-just2200@yandex.ru>
2019-07-25 11:00:54 +0300

    fix admin page

--- learning_app/admin.py
+++ learning_app/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from learning_app.models import Topic, Entry
 
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('name_topic', 'text', 'date_add')
+    list_display_links = ('text', )
+    search_fields = ('text', )
+
+
 admin.site.register(Topic)
-admin.site.register(Entry)
+admin.site.register(Entry, EntryAdmin)
--- learning_app/models.py
+++ learning_app/models.py
@@ -3,10 +3,14 @@ from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """ Тема, которую изучает пользователь. """
-    name = models.CharField(max_length=200)
-    date_add = models.DateField(auto_now_add=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    public = models.BooleanField(default=False)
+    name = models.CharField(max_length=200, verbose_name = 'Тема')
+    date_add = models.DateField(auto_now_add=True, verbose_name ='Дата добавления')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,verbose_name = 'пользователь' )
+    public = models.BooleanField(default=False,verbose_name = 'Публичная')
+
+    class Meta:
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
 
     def __str__(self):
 
@@ -15,12 +19,13 @@ class Topic(models.Model):
 
 class Entry(models.Model):
     """Информация изученная пользователем по теме"""
-    name_topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    text = models.TextField()
-    date_add = models.DateField(auto_now_add=True)
+    name_topic = models.ForeignKey(Topic, on_delete=models.CASCADE, verbose_name = 'Тема')
+    text = models.TextField(verbose_name = 'Запись')
+    date_add = models.DateField(auto_now_add=True, verbose_name = 'Дата добавления')
 
     class Meta:
-        verbose_name_plural = 'entries'
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
 
     def __str__(self):
 
